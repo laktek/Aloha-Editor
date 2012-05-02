4bc5de257da3ca8989d19a99151effa118b9d035
Lakshan Perera <lakshan@web2media.net>
2012-05-02 07:32:47 +0530

    Modified the formatting plugin to use the jQueryUI elements

--- src/plugins/common/format/lib/format-plugin.js
+++ src/plugins/common/format/lib/format-plugin.js
@@ -5,10 +5,20 @@
 * Licensed unter the terms of http://www.aloha-editor.com/license.html
 */
 
-define(
-['aloha', 'aloha/plugin', 'aloha/jquery', 'aloha/floatingmenu', 'i18n!format/nls/i18n', 'i18n!aloha/nls/i18n', 'aloha/console',
- 		'css!format/css/format.css'],
-function(Aloha, Plugin, jQuery, FloatingMenu, i18n, i18nCore) {
+define([
+  "aloha",
+  "aloha/plugin",
+  "aloha/jquery",
+  "ui/component",
+  "ui/surface",
+	"ui/toggleCommandButton",
+	"ui/multiSplit",
+  "i18n!format/nls/i18n",
+  "i18n!aloha/nls/i18n",
+  'aloha/console',
+  'css!format/css/format.css'
+],
+function(Aloha, Plugin, jQuery, Component, Surface, ToggleCommandButton, MultiSplit, i18n, i18nCore) {
 	"use strict";
 	var
 		GENTICS = window.GENTICS;
@@ -23,326 +33,135 @@ function(Aloha, Plugin, jQuery, FloatingMenu, i18n, i18nCore) {
 		languages: ['en', 'de', 'fr', 'eo', 'fi', 'ru', 'it', 'pl'],
 
 		/**
-		 * default button configuration
+		 * default inline formatting buttons
 		 */
-		config: [ 'b', 'i', 's', 'u', 'sub','sup', 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'pre', 'removeFormat'],
+     inlineFormattingButtons: [ "bold", "italic", "strikethrough", "subscript", "superscript", "underline" ],
+
+    /**
+		 * default block formatting options 
+		 */
+    formatBlockButtons: [ "p", "h1", "h2", "h3", "h4", "h5", "h6", "pre" ],
+
+    /**
+		 * set the default formats according to the removeformat command 
+		 */
+		formatsToRemove: ["abbr", "acronym", "b", "bdi", "bdo", "big", "blink", "cite", "code", "dfn", "em", "font", "i", "ins", "kbd", "mark", "nobr", "q", "s", "samp", "small", "span", "strike", "strong", "sub", "sup", "tt", "u", "var"],
 
 		/**
 		 * Initialize the plugin and set initialize flag on true
 		 */
 		init: function () {
 			// Prepare
-			var me = this;
+			var that = this;
 
-			this.initButtons();
+      //init inline formatting buttons
+			that.initInlineFormattingButtons();
 
-			// apply specific configuration if an editable has been activated
-			Aloha.bind('aloha-editable-activated',function (e, params) {
-				//debugger;
-				me.applyButtonConfig(params.editable.obj);
-			});
-
-			/*
-			Aloha.defaults.supports = jQuery.merge(Aloha.defaults.supports, {
-					elements: [ 'strong', 'em', 'b', 'i','del','sub','sup', 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'pre' ]
-			});
-			*/
-
+      // initialize formatting block
+      that.initFormatBlock();
 		},
 
-		/**
-		 * applys a configuration specific for an editable
-		 * buttons not available in this configuration are hidden
-		 * @param {Object} id of the activated editable
-		 * @return void
-		 */
-		applyButtonConfig: function (obj) {
+    /**
+      * Add inline formatting buttons
+		  */
+		initInlineFormattingButtons: function () {
+			var that = this;
 
-			var config = this.getEditableConfig(obj),
-				button, i, len;
+      // The bold component is a [toggleCommandButton](toggleCommandButton.html) that ties into the bold command.
+      // The button will automatically update its state whenever the selection changes
+      // and it will apply or remove the bold styling when the button is clicked.
+      // This functionality comes from the toggleButton which knows how to hook into
+      // the associated command.
+			jQuery.each(that.inlineFormattingButtons, function( i, command ) {
+        Component.define( command, ToggleCommandButton, {
+          command: command,
+          label: i18n.t( "button." + command + ".label" ),
+          iconOnly: true,
+          icon: "aloha-icon aloha-icon-" + command
+        });
+      });
+    },
 
-			// now iterate all buttons and show/hide them according to the config
-			for ( button in this.buttons) {
-				if (jQuery.inArray(button, config) != -1) {
-					this.buttons[button].button.show();
-				} else {
-					this.buttons[button].button.hide();
-				}
-			}
+    initFormatBlock: function(){
+      var that = this;
 
-			// and the same for multisplit items
-			len = this.multiSplitItems.length;
-			for (i = 0; i < len; i++) {
-				if (jQuery.inArray(this.multiSplitItems[i].name, config) != -1) {
-					this.multiSplitButton.showItem(this.multiSplitItems[i].name);
-				} else {
-					this.multiSplitButton.hideItem(this.multiSplitItems[i].name);
-				}
-			}
-		},
+      /**
+       * Format block component
+       * @class
+       * @extends {MultiSplit}
+       */
+      var FormatBlock = Component.define( "formatBlock", MultiSplit, {
+        /**
+         * Gets the buttons for the multi split menu
+         * @returns {Array.<Object>}
+         */
+        getButtons: function() {
+          // TODO: Finalize on how the settings should be given
+          return jQuery.map( that.formatBlockButtons, function( item ) {
+            return FormatBlock._buttons[ item ];
+          });
+        },
 
-		/**
-		 * initialize the buttons and register them on floating menu
-		 * @param event event object
-		 * @param editable current editable object
-		 */
-		initButtons: function () {
-			var
-				scope = 'Aloha.continuoustext',
-				that = this;
+        /**
+         * Gets the items for bottom of the multi split menu
+         * @returns {Array.<Object>}
+         */
+        getItems: function() {
+          var formatBlock = this;
+          return [{
+            label: i18n.t( "button.removeFormatting.label" ),
+            click: function() {
+              that.removeFormatting( Surface.range, this.editable );
+            }
+          }];
+        }
+      });
 
-       var command_for = {
-         'b': 'bold',
-         'i': 'italic',
-         's': 'strikethrough',
-         'u': 'underline',
-         'sub': 'subscript',
-         'sup': 'superscript' 
-       };
-
-			// reset
-			this.buttons = {};
-
-			// collect the multisplit items here
-			this.multiSplitItems = [];
-			//this.multiSplitButton;
-
-			//iterate configuration array an push buttons to buttons array
-			jQuery.each(this.config, function(j, button) {
-				switch( button ) {
-          // inline formatting commands
-          case 'b':
-          case 'i':
-          case 's':
-          case 'u':
-          case 'sub':
-          case 'sup':
-            that.buttons[button] = {
-              'button' : new Aloha.ui.Button({
-                'name' : button,
-                'iconClass' : 'aloha-button aloha-button-' + button,
-                'size' : 'small',
-                'onclick' : function () {
-                  Aloha.execCommand(command_for[button], false);
-                },
-                'tooltip' : i18n.t('button.' + button + '.tooltip'),
-                'toggle' : true
-              }), 
-              'markup' : jQuery('<'+button+'></'+button+'>'),
-              'command': command_for[button]
-            };
-
-						FloatingMenu.addButton(
-							scope,
-							that.buttons[button].button,
-							i18nCore.t('floatingmenu.tab.format'),
-							1
-						);
-						break;
-
-          /*
-					// text level semantics:
-					case 'em':
-					case 'strong':
-					case 'cite':
-					case 'q':
-					case 'code':
-					case 'abbr':
-					case 'del':
-						that.buttons[button] = {'button' : new Aloha.ui.Button({
-							'name' : button,
-							'iconClass' : 'aloha-button aloha-button-' + button,
-							'size' : 'small',
-							'onclick' : function () {
-								var
-									markup = jQuery('<'+button+'></'+button+'>'),
-									rangeObject = Aloha.Selection.rangeObject,
-									foundMarkup;
-
-								// check whether the markup is found in the range (at the start of the range)
-								foundMarkup = rangeObject.findMarkup(function() {
-									return this.nodeName.toLowerCase() == markup.get(0).nodeName.toLowerCase();
-								}, Aloha.activeEditable.obj);
-
-								if (foundMarkup) {
-									// remove the markup
-									if (rangeObject.isCollapsed()) {
-										// when the range is collapsed, we remove exactly the one DOM element
-										GENTICS.Utils.Dom.removeFromDOM(foundMarkup, rangeObject, true);
-									} else {
-										// the range is not collapsed, so we remove the markup from the range
-										GENTICS.Utils.Dom.removeMarkup(rangeObject, markup, Aloha.activeEditable.obj);
-									}
-								} else {
-									// when the range is collapsed, extend it to a word
-									if (rangeObject.isCollapsed()) {
-										GENTICS.Utils.Dom.extendToWord(rangeObject);
-									}
-
-									// add the markup
-									GENTICS.Utils.Dom.addMarkup(rangeObject, markup);
-								}
-								// select the modified range
-								rangeObject.select();
-								return false;
-							},
-							'tooltip' : i18n.t('button.' + button + '.tooltip'),
-							'toggle' : true
-						}), 'markup' : jQuery('<'+button+'></'+button+'>')};
-
-						FloatingMenu.addButton(
-							scope,
-							that.buttons[button].button,
-							i18nCore.t('floatingmenu.tab.format'),
-							1
-						);
-						break;
-          */
-
-					case 'p':
-					case 'h1':
-					case 'h2':
-					case 'h3':
-					case 'h4':
-					case 'h5':
-					case 'h6':
-					case 'pre':
-						that.multiSplitItems.push({
-							'name' : button,
-							'tooltip' : i18n.t('button.' + button + '.tooltip'),
-							'iconClass' : 'aloha-button ' + i18n.t('aloha-button-' + button),
-							'markup' : jQuery('<'+button+'></'+button+'>'),
-							'click' : function() {
-								//Aloha.Selection.changeMarkupOnSelection(jQuery('<' + button + '></' + button + '>'));
-                Aloha.execCommand('formatBlock', false, button);
-							}
-						});
-						break;
-
-					// wide multisplit buttons
-					case 'removeFormat':
-						that.multiSplitItems.push({
-							'name' : button,
-							'text' : i18n.t('button.' + button + '.text'),
-							'tooltip' : i18n.t('button.' + button + '.tooltip'),
-							'iconClass' : 'aloha-button aloha-button-' + button,
-							'wide' : true,
-							'click' : function() {
-								that.removeFormat();
-							}
-						});
-						break;
-					//no button defined
-					default:
-						Aloha.log('warn', this, 'Button "' + button + '" is not defined');
-						break;
-				}
-			});
-
-			if (this.multiSplitItems.length > 0) {
-				this.multiSplitButton = new Aloha.ui.MultiSplitButton({
-					'name' : 'phrasing',
-					'items' : this.multiSplitItems
-				});
-				FloatingMenu.addButton(
-					scope,
-					this.multiSplitButton,
-					i18nCore.t('floatingmenu.tab.format'),
-					3
-				);
-			}
-
-			// add the event handler for selection change
-			Aloha.bind('aloha-selection-changed',function(event,rangeObject){
-				// iterate over all buttons
-				var
-					statusWasSet = false, effectiveMarkup,
-					foundMultiSplit, i, j, multiSplitItem;
-
-				jQuery.each(that.buttons, function(index, button) {
-					// statusWasSet = false;
-					// for ( i = 0; i < rangeObject.markupEffectiveAtStart.length; i++) {
-					// 	effectiveMarkup = rangeObject.markupEffectiveAtStart[ i ];
-					// 	if (Aloha.Selection.standardTextLevelSemanticsComparator(effectiveMarkup, button.markup)) {
-					// 		button.button.setPressed(true);
-					// 		statusWasSet = true;
-					// 	}
-					// }
-					// if (!statusWasSet) {
-					// 	button.button.setPressed(false);
-					// }
-          if(Aloha.queryCommandState(button.command)){
-            button.button.setPressed(true);
+      /**
+       * Settings for all format block buttons
+       * @type {Array.<Object>}
+       */
+      FormatBlock._buttons = {};
+      jQuery.each(that.formatBlockButtons, function( i, block ) {
+        FormatBlock._buttons[ block ] = {
+          label: i18n.t( "button." + block + ".label" ),
+          icon: "aloha-large-icon-" + block,
+          click: function() {
+            Aloha.execCommand( "formatBlock", false, block, Surface.range );
+          },
+          isActive: function() {
+            return Aloha.queryCommandValue( "formatBlock" ) === block;
           }
-          else {
-            button.button.setPressed(false);
-          }
-				});
-
-				if (that.multiSplitItems.length > 0) {
-					// foundMultiSplit = false;
-
-					// // iterate over the markup elements
-					// for ( i = 0; i < rangeObject.markupEffectiveAtStart.length && !foundMultiSplit; i++) {
-					// 	effectiveMarkup = rangeObject.markupEffectiveAtStart[ i ];
-
-					// 	for ( j = 0; j < that.multiSplitItems.length && !foundMultiSplit; j++) {
-					// 		multiSplitItem = that.multiSplitItems[j];
-
-					// 		if (!multiSplitItem.markup) {
-					// 			continue;
-					// 		}
-
-					// 		// now check whether one of the multiSplitItems fits to the effective markup
-					// 		if (Aloha.Selection.standardTextLevelSemanticsComparator(effectiveMarkup, multiSplitItem.markup)) {
-					// 			that.multiSplitButton.setActiveItem(multiSplitItem.name);
-					// 			foundMultiSplit = true;
-					// 		}
-					// 	}
-					// }
-
-					// if (!foundMultiSplit) {
-					// 	that.multiSplitButton.setActiveItem(null);
-					// }
-
-          var command_value = Aloha.queryCommandValue('formatBlock');
-					that.multiSplitButton.setActiveItem(command_value);
-
-				}
-			});
-
-		},
+        };
+      });
+    },
 
 		/**
 		 * Removes all formatting from the current selection.
 		 */
-		removeFormat: function() {
-			//var formats = [ 'strong', 'em', 'b', 'i', 'cite', 'q', 'code', 'abbr', 'del', 'sub', 'sup'],
+		removeFormatting: function(range) {
       // set the default formats according to the removeformat command.
       var formats = ["abbr", "acronym", "b", "bdi", "bdo", "big", "blink", "cite", "code", "dfn", "em", "font", "i", "ins", "kbd", "mark", "nobr", "q", "s", "samp", "small", "span", "strike", "strong", "sub", "sup", "tt", "u", "var"],
-				rangeObject = Aloha.Selection.rangeObject,
-				i;
+          rangeObject = new GENTICS.Utils.RangeObject( range || Aloha.getSelection().getRangeAt( 0 ) );
 			
+      // TODO: Finalize on how to pass the settings to the plugin
 			// formats to be removed by the removeFormat button may now be configured using Aloha.settings.plugins.format.removeFormats = ['b', 'strong', ...]
-			if (this.settings.removeFormats) {
-				formats = this.settings.removeFormats;
-			}
+			//if (this.settings.removeFormats) {
+		  //		formats = this.settings.removeFormats;
+			//}
 
 			if (rangeObject.isCollapsed()) {
 				return;
 			}
 
-			// for (i = 0; i < formats.length; i++) {
-			// 	GENTICS.Utils.Dom.removeMarkup(rangeObject, jQuery('<' + formats[i] + '></' + formats[i] + '>'), Aloha.activeEditable.obj);
-			// }
-
       // run removeformat command on the selected range
-      Aloha.execCommand("removeformat", false, formats);
+      // TODO: check the difference between range and rangeObject
+      Aloha.execCommand("removeformat", false, formats, range);
 
 			// select the modified range
 			rangeObject.select();
-			// TODO: trigger event - removed Format
 
+			// TODO: trigger event - removed Format
 		},
 
 		/**
