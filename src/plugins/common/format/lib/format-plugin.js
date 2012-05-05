e11d506c9364de25b71c1adde2e83236bf203094
Petro Salema <p.salema@gentics.com>
2012-05-05 15:05:07 +0200

    More refactoring in format-plugin.js .  Moving functions that don't need to be exposed into private scope.

--- src/plugins/common/format/lib/format-plugin.js
+++ src/plugins/common/format/lib/format-plugin.js
@@ -1,3 +1,4 @@
+/*global define:true*/
 /*!
  * Aloha Editor
  * Author & Copyright (c) 2010-2012 Gentics Software GmbH
@@ -19,13 +20,78 @@ define([
 	'css!format/css/format.css'
 ],
 function( Aloha, Plugin, jQuery, Component, Surface, ToggleCommandButton,
-          MultiSplit, i18n, i18nCore ) {
+          MultiSplitButton, i18n, i18nCore ) {
 	'use strict';
 
 	/**
-	 * @type {object} Local reference to the GENTICS namespace.
+	 * @type {Array.<string>} Default inline formatting buttons.
+	 * @const
+	 * @private
 	 */
-	var GENTICS = window.GENTICS;
+	var INLINE_FORMATTING_BUTTONS = [ 'bold', 'italic', 'strikethrough',
+		'subscript', 'superscript', 'underline' ];
+
+	/**
+	 * @type {Array.<string>} Default block formatting options.
+	 * @const
+	 * @private
+	 */
+	var FORMAT_BLOCK_BUTTONS = [ 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+		'pre' ];
+
+	/**
+	 * @type {Array.<string>} Set the default formats according to the
+	 *                        `removeFormatting()' command.
+	 * @const
+	 * @private
+	 */
+	var FORMATS_TO_REMOVE = [
+		'abbr', 'acronym',
+		'b', 'bdi', 'bdo', 'big', 'blink',
+		'cite', 'code',
+		'dfn', 'em',
+		'font',
+		'i', 'ins',
+		'kbd',
+		'mark',
+		'nobr',
+		'q',
+		's', 'samp', 'small', 'span', 'strike', 'strong', 'sub', 'sup',
+		'tt',
+		'u',
+		'var'
+	];
+
+	/**
+	 * Add inline formatting buttons.
+	 *
+	 * @private
+	 */
+	function initInlineFormattingButtons() {
+		// The bold component is a
+		// [toggleCommandButton](toggleCommandButton.html) that ties into the
+		// bold command.  The button will automatically update its state
+		// whenever the selection changes and it will apply or remove the bold
+		// styling when the button is clicked.  This functionality comes from
+		// the toggleButton which knows how to hook into the associated
+		// command.
+		jQuery.each( INLINE_FORMATTING_BUTTONS, function( i, command ) {
+			Component.define( command, ToggleCommandButton, {
+				command  : command,
+				label    : i18n.t( 'button.' + command + '.label' ),
+				iconOnly : true,
+				icon     : 'aloha-icon aloha-icon-' + command
+			});
+		});
+	}
+
+
+	/**
+	 * Local reference to the `RangeObject' constructor.
+	 * @constructor
+	 * @param {Aloha.Range} range.
+	 */
+	var RangeObject = window.GENTICS.Utils.RangeObject;
 
 	return Plugin.create( 'format', {
 
@@ -36,81 +102,27 @@ function( Aloha, Plugin, jQuery, Component, Surface, ToggleCommandButton,
 		languages: [ 'en', 'de', 'fr', 'eo', 'fi', 'ru', 'it', 'pl' ],
 
 		/**
-		 * @type {Array.<string>} Default inline formatting buttons.
-		 * @const
-		 */
-		 inlineFormattingButtons: [ 'bold', 'italic', 'strikethrough',
-		 	'subscript', 'superscript', 'underline' ],
-
-		/**
-		 * @type {Array.<string>} Default block formatting options.
-		 * @const
-		 */
-		formatBlockButtons: [ 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'pre' ],
-
-		/**
-		 * @type {Array.<string>} Set the default formats according to the
-		 *                        removeformat command.
-		 * @const
-		 */
-		formatsToRemove: [
-			'abbr', 'acronym',
-			'b', 'bdi', 'bdo', 'big', 'blink',
-			'cite', 'code',
-			'dfn', 'em',
-			'font',
-			'i', 'ins',
-			'kbd',
-			'mark',
-			'nobr',
-			'q',
-			's', 'samp', 'small', 'span', 'strike', 'strong', 'sub', 'sup',
-			'tt',
-			'u',
-			'var'
-		],
-
-		/**
 		 * Initialize the plugin and set initialize flag to true.
 		 */
 		init: function() {
-			this.initInlineFormattingButtons();
-			this.initFormatBlock();
+			initInlineFormattingButtons();
+			this._initFormatBlock();
 		},
 
 		/**
-		 * Add inline formatting buttons.
-		 *
-		 * @todo: Make this private function, not a method of the plugin.
+		 * @private
 		 */
-		initInlineFormattingButtons: function() {
-			// The bold component is a
-			// [toggleCommandButton](toggleCommandButton.html) that ties into
-			// the bold command.  The button will automatically update its
-			// state whenever the selection changes and it will apply or remove
-			// the bold styling when the button is clicked.  This functionality
-			// comes from the toggleButton which knows how to hook into the
-			// associated command.
-			jQuery.each( this.inlineFormattingButtons, function( i, command ) {
-				Component.define( command, ToggleCommandButton, {
-					command  : command,
-					label    : i18n.t( 'button.' + command + '.label' ),
-					iconOnly : true,
-					icon     : 'aloha-icon aloha-icon-' + command
-				});
-			});
-		},
-
-		initFormatBlock: function() {
+		_initFormatBlock: function() {
 			var plugin = this;
 
 			/**
 			 * Format block component
 			 *
 			 * @class
-			 * @extends {MultiSplit}
+			 * @extends {Aloha.ui.MultiSplitButton}
 			 */
-			var FormatBlock = Component.define( 'formatBlock', MultiSplit, {
+			var FormatBlock = Component.define( 'formatBlock', MultiSplitButton, {
+
 				/**
 				 * Gets the buttons for the multi split menu.
 				 *
@@ -118,7 +130,7 @@ function( Aloha, Plugin, jQuery, Component, Surface, ToggleCommandButton,
 				 */
 				getButtons: function() {
 					// TODO: Finalize on how the settings should be given
-					return jQuery.map( plugin.formatBlockButtons, function( item ) {
+					return jQuery.map( FORMAT_BLOCK_BUTTONS, function( item ) {
 						return FormatBlock._buttons[ item ];
 					});
 				},
@@ -129,8 +141,6 @@ function( Aloha, Plugin, jQuery, Component, Surface, ToggleCommandButton,
 				 * @returns {Array.<Object>}
 				 */
 				getItems: function() {
-					var formatBlock = this;
-
 					return [{
 						label: i18n.t( 'button.removeFormatting.label' ),
 						click: function() {
@@ -141,13 +151,14 @@ function( Aloha, Plugin, jQuery, Component, Surface, ToggleCommandButton,
 			});
 
 			/**
-			 * Settings for all format block buttons.
-			 *
-			 * @type {Array.<Object>}
+			 * @type {Array.<Object>} Settings for all format block buttons.
+			 *                        Class variable.
+			 * @private
+			 * @static
 			 */
 			FormatBlock._buttons = {};
 
-			jQuery.each( this.formatBlockButtons, function( i, block ) {
+			jQuery.each( FORMAT_BLOCK_BUTTONS, function( i, block ) {
 				FormatBlock._buttons[ block ] = {
 					label : i18n.t( 'button.' + block + '.label' ),
 					icon  : 'aloha-large-icon-' + block,
@@ -156,6 +167,7 @@ function( Aloha, Plugin, jQuery, Component, Surface, ToggleCommandButton,
 							Surface.range );
 					},
 					isActive: function() {
+						console.log('isActivate called');
 						return ( Aloha.queryCommandValue( 'formatBlock' ) ===
 							block );
 					}
@@ -167,15 +179,8 @@ function( Aloha, Plugin, jQuery, Component, Surface, ToggleCommandButton,
 		 * Removes all formatting from the current selection.
 		 */
 		removeFormatting: function(range) {
-			// set the default formats according to the removeformat command.
-			var formats = [ 'abbr', 'acronym', 'b', 'bdi', 'bdo', 'big',
-				'blink', 'cite', 'code', 'dfn', 'em', 'font', 'i', 'ins',
-				'kbd', 'mark', 'nobr', 'q', 's', 'samp', 'small', 'span',
-				'strike', 'strong', 'sub', 'sup', 'tt', 'u', 'var'];
-			
-			// Import getRangeObject;
-			var rangeObject = new GENTICS.Utils.RangeObject( range
-				|| Aloha.getSelection().getRangeAt( 0 ) );
+			var rangeObject = new RangeObject( range ||
+				Aloha.getSelection().getRangeAt( 0 ) );
 
 			// TODO: Finalize on how to pass the settings to the plugin formats
 			// to be removed by the removeFormat button may now be configured
@@ -189,9 +194,8 @@ function( Aloha, Plugin, jQuery, Component, Surface, ToggleCommandButton,
 				return;
 			}
 
-			// run removeformat command on the selected range
 			// TODO: check the difference between range and rangeObject
-			Aloha.execCommand('removeformat', false, formats, range);
+			Aloha.execCommand('removeformat', false, FORMATS_TO_REMOVE , range);
 
 			rangeObject.select();
 
