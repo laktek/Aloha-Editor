4c01364ea8026a0b7a9ebf442c80870a1ce2ff11
Petro Salema <p.salema@gentics.com>
2012-05-05 14:12:38 +0200

    Refactoring, and cleaning up format-plugin.js

--- src/plugins/common/format/lib/format-plugin.js
+++ src/plugins/common/format/lib/format-plugin.js
@@ -1,175 +1,210 @@
 /*!
-* Aloha Editor
-* Author & Copyright (c) 2010 Gentics Software GmbH
-* aloha-sales@gentics.com
-* Licensed unter the terms of http://www.aloha-editor.com/license.html
-*/
+ * Aloha Editor
+ * Author & Copyright (c) 2010-2012 Gentics Software GmbH
+ * aloha-sales@gentics.com
+ * Licensed unter the terms of http://www.aloha-editor.com/license.html
+ */
 
 define([
-  "aloha",
-  "aloha/plugin",
-  "aloha/jquery",
-  "ui/component",
-  "ui/surface",
-	"ui/toggleCommandButton",
-	"ui/multiSplit",
-  "i18n!format/nls/i18n",
-  "i18n!aloha/nls/i18n",
-  'aloha/console',
-  'css!format/css/format.css'
+	'aloha',
+	'aloha/plugin',
+	'aloha/jquery',
+	'ui/component',
+	'ui/surface',
+	'ui/toggleCommandButton',
+	'ui/multiSplit',
+	'i18n!format/nls/i18n',
+	'i18n!aloha/nls/i18n',
+	'aloha/console',
+	'css!format/css/format.css'
 ],
-function(Aloha, Plugin, jQuery, Component, Surface, ToggleCommandButton, MultiSplit, i18n, i18nCore) {
-	"use strict";
-	var
-		GENTICS = window.GENTICS;
+function( Aloha, Plugin, jQuery, Component, Surface, ToggleCommandButton,
+          MultiSplit, i18n, i18nCore ) {
+	'use strict';
 
 	/**
-	 * register the plugin with unique name
+	 * @type {object} Local reference to the GENTICS namespace.
 	 */
-	return Plugin.create('format', {
-		/**
-		 * Configure the available languages
-		 */
-		languages: ['en', 'de', 'fr', 'eo', 'fi', 'ru', 'it', 'pl'],
+	var GENTICS = window.GENTICS;
+
+	return Plugin.create( 'format', {
 
 		/**
-		 * default inline formatting buttons
+		 * @type {Array.<string>} Available i18n languages.
+		 * @const
 		 */
-     inlineFormattingButtons: [ "bold", "italic", "strikethrough", "subscript", "superscript", "underline" ],
-
-    /**
-		 * default block formatting options 
-		 */
-    formatBlockButtons: [ "p", "h1", "h2", "h3", "h4", "h5", "h6", "pre" ],
-
-    /**
-		 * set the default formats according to the removeformat command 
-		 */
-		formatsToRemove: ["abbr", "acronym", "b", "bdi", "bdo", "big", "blink", "cite", "code", "dfn", "em", "font", "i", "ins", "kbd", "mark", "nobr", "q", "s", "samp", "small", "span", "strike", "strong", "sub", "sup", "tt", "u", "var"],
+		languages: [ 'en', 'de', 'fr', 'eo', 'fi', 'ru', 'it', 'pl' ],
 
 		/**
-		 * Initialize the plugin and set initialize flag on true
+		 * @type {Array.<string>} Default inline formatting buttons.
+		 * @const
 		 */
-		init: function () {
-			// Prepare
-			var that = this;
+		 inlineFormattingButtons: [ 'bold', 'italic', 'strikethrough',
+		 	'subscript', 'superscript', 'underline' ],
 
-      //init inline formatting buttons
-			that.initInlineFormattingButtons();
+		/**
+		 * @type {Array.<string>} Default block formatting options.
+		 * @const
+		 */
+		formatBlockButtons: [ 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'pre' ],
 
-      // initialize formatting block
-      that.initFormatBlock();
+		/**
+		 * @type {Array.<string>} Set the default formats according to the
+		 *                        removeformat command.
+		 * @const
+		 */
+		formatsToRemove: [
+			'abbr', 'acronym',
+			'b', 'bdi', 'bdo', 'big', 'blink',
+			'cite', 'code',
+			'dfn', 'em',
+			'font',
+			'i', 'ins',
+			'kbd',
+			'mark',
+			'nobr',
+			'q',
+			's', 'samp', 'small', 'span', 'strike', 'strong', 'sub', 'sup',
+			'tt',
+			'u',
+			'var'
+		],
+
+		/**
+		 * Initialize the plugin and set initialize flag to true.
+		 */
+		init: function() {
+			this.initInlineFormattingButtons();
+			this.initFormatBlock();
 		},
 
-    /**
-      * Add inline formatting buttons
-		  */
-		initInlineFormattingButtons: function () {
-			var that = this;
+		/**
+		 * Add inline formatting buttons.
+		 *
+		 * @todo: Make this private function, not a method of the plugin.
+		 */
+		initInlineFormattingButtons: function() {
+			// The bold component is a
+			// [toggleCommandButton](toggleCommandButton.html) that ties into
+			// the bold command.  The button will automatically update its
+			// state whenever the selection changes and it will apply or remove
+			// the bold styling when the button is clicked.  This functionality
+			// comes from the toggleButton which knows how to hook into the
+			// associated command.
+			jQuery.each( this.inlineFormattingButtons, function( i, command ) {
+				Component.define( command, ToggleCommandButton, {
+					command  : command,
+					label    : i18n.t( 'button.' + command + '.label' ),
+					iconOnly : true,
+					icon     : 'aloha-icon aloha-icon-' + command
+				});
+			});
+		},
 
-      // The bold component is a [toggleCommandButton](toggleCommandButton.html) that ties into the bold command.
-      // The button will automatically update its state whenever the selection changes
-      // and it will apply or remove the bold styling when the button is clicked.
-      // This functionality comes from the toggleButton which knows how to hook into
-      // the associated command.
-			jQuery.each(that.inlineFormattingButtons, function( i, command ) {
-        Component.define( command, ToggleCommandButton, {
-          command: command,
-          label: i18n.t( "button." + command + ".label" ),
-          iconOnly: true,
-          icon: "aloha-icon aloha-icon-" + command
-        });
-      });
-    },
+		initFormatBlock: function() {
+			var plugin = this;
 
-    initFormatBlock: function(){
-      var that = this;
+			/**
+			 * Format block component
+			 *
+			 * @class
+			 * @extends {MultiSplit}
+			 */
+			var FormatBlock = Component.define( 'formatBlock', MultiSplit, {
+				/**
+				 * Gets the buttons for the multi split menu.
+				 *
+				 * @returns {Array.<Object>}
+				 */
+				getButtons: function() {
+					// TODO: Finalize on how the settings should be given
+					return jQuery.map( plugin.formatBlockButtons, function( item ) {
+						return FormatBlock._buttons[ item ];
+					});
+				},
 
-      /**
-       * Format block component
-       * @class
-       * @extends {MultiSplit}
-       */
-      var FormatBlock = Component.define( "formatBlock", MultiSplit, {
-        /**
-         * Gets the buttons for the multi split menu
-         * @returns {Array.<Object>}
-         */
-        getButtons: function() {
-          // TODO: Finalize on how the settings should be given
-          return jQuery.map( that.formatBlockButtons, function( item ) {
-            return FormatBlock._buttons[ item ];
-          });
-        },
+				/**
+				 * Gets the items for bottom of the multi split menu.
+				 *
+				 * @returns {Array.<Object>}
+				 */
+				getItems: function() {
+					var formatBlock = this;
 
-        /**
-         * Gets the items for bottom of the multi split menu
-         * @returns {Array.<Object>}
-         */
-        getItems: function() {
-          var formatBlock = this;
-          return [{
-            label: i18n.t( "button.removeFormatting.label" ),
-            click: function() {
-              that.removeFormatting( Surface.range, this.editable );
-            }
-          }];
-        }
-      });
+					return [{
+						label: i18n.t( 'button.removeFormatting.label' ),
+						click: function() {
+							plugin.removeFormatting( Surface.range, this.editable );
+						}
+					}];
+				}
+			});
 
-      /**
-       * Settings for all format block buttons
-       * @type {Array.<Object>}
-       */
-      FormatBlock._buttons = {};
-      jQuery.each(that.formatBlockButtons, function( i, block ) {
-        FormatBlock._buttons[ block ] = {
-          label: i18n.t( "button." + block + ".label" ),
-          icon: "aloha-large-icon-" + block,
-          click: function() {
-            Aloha.execCommand( "formatBlock", false, block, Surface.range );
-          },
-          isActive: function() {
-            return Aloha.queryCommandValue( "formatBlock" ) === block;
-          }
-        };
-      });
-    },
+			/**
+			 * Settings for all format block buttons.
+			 *
+			 * @type {Array.<Object>}
+			 */
+			FormatBlock._buttons = {};
+
+			jQuery.each( this.formatBlockButtons, function( i, block ) {
+				FormatBlock._buttons[ block ] = {
+					label : i18n.t( 'button.' + block + '.label' ),
+					icon  : 'aloha-large-icon-' + block,
+					click : function() {
+						Aloha.execCommand( 'formatBlock', false, block,
+							Surface.range );
+					},
+					isActive: function() {
+						return ( Aloha.queryCommandValue( 'formatBlock' ) ===
+							block );
+					}
+				};
+			});
+		},
 
 		/**
 		 * Removes all formatting from the current selection.
 		 */
 		removeFormatting: function(range) {
-      // set the default formats according to the removeformat command.
-      var formats = ["abbr", "acronym", "b", "bdi", "bdo", "big", "blink", "cite", "code", "dfn", "em", "font", "i", "ins", "kbd", "mark", "nobr", "q", "s", "samp", "small", "span", "strike", "strong", "sub", "sup", "tt", "u", "var"],
-          rangeObject = new GENTICS.Utils.RangeObject( range || Aloha.getSelection().getRangeAt( 0 ) );
+			// set the default formats according to the removeformat command.
+			var formats = [ 'abbr', 'acronym', 'b', 'bdi', 'bdo', 'big',
+				'blink', 'cite', 'code', 'dfn', 'em', 'font', 'i', 'ins',
+				'kbd', 'mark', 'nobr', 'q', 's', 'samp', 'small', 'span',
+				'strike', 'strong', 'sub', 'sup', 'tt', 'u', 'var'];
 			
-      // TODO: Finalize on how to pass the settings to the plugin
-			// formats to be removed by the removeFormat button may now be configured using Aloha.settings.plugins.format.removeFormats = ['b', 'strong', ...]
+			// Import getRangeObject;
+			var rangeObject = new GENTICS.Utils.RangeObject( range
+				|| Aloha.getSelection().getRangeAt( 0 ) );
+
+			// TODO: Finalize on how to pass the settings to the plugin formats
+			// to be removed by the removeFormat button may now be configured
+			// using Aloha.settings.plugins.format.removeFormats = ['b',
+			// 'strong', ...]
 			//if (this.settings.removeFormats) {
-		  //		formats = this.settings.removeFormats;
+			//		formats = this.settings.removeFormats;
 			//}
 
 			if (rangeObject.isCollapsed()) {
 				return;
 			}
 
-      // run removeformat command on the selected range
-      // TODO: check the difference between range and rangeObject
-      Aloha.execCommand("removeformat", false, formats, range);
+			// run removeformat command on the selected range
+			// TODO: check the difference between range and rangeObject
+			Aloha.execCommand('removeformat', false, formats, range);
 
-			// select the modified range
 			rangeObject.select();
 
 			// TODO: trigger event - removed Format
 		},
 
 		/**
-		* toString method
-		* @return string
-		*/
-		toString: function () {
+		 * @return {string} A string representation of this plugin instance.
+		 */
+		toString: function() {
 			return 'format';
 		}
+
 	});
+
 });
